4434dc2d6ce0164168273ecd115a96c41f87a5fb
hemraj <hj130640@gmail.com>
2026-05-08 09:21:40 +0545

    Card style updated

--- src/app/services/training/page.tsx
+++ src/app/services/training/page.tsx
@@ -1,121 +1,112 @@
 'use client';
-import ServiceCard from '@/components/cards/serviceCard'
 import Data from '@/api/data.json'
 import { useEffect, useState } from 'react'
+import TrainingCard from '@/components/cards/trainingCard';
+import BlockTitle from '@/components/blockTitle/blockTitle';
 
 const Training = () => {
   const [activeFilter, setActiveFilter] = useState(
     Data.accordionButtonData?.[0]?.accordionBtn || 'All Programs'
   )
-  const filteredData =
+  const filteredTrainingData =
     activeFilter === 'All Programs'
-      ? Data.trainingPageData
-      : Data.trainingPageData.filter(
+      ? Data.trainingData
+      : Data.trainingData.filter(
         (item) => item.category === activeFilter
       )
 
   // Pagination Code
-  const [currentPage, setCurrentPage] = useState(1)
+  const [currentPage, setCurrentPage] = useState<number>(1)
   const itemsPerPage = 6
-  const totalPages = Math.ceil(filteredData.length / itemsPerPage)
+  const totalPages = Math.ceil(filteredTrainingData.length / itemsPerPage)
 
-  const paginatedData = filteredData.slice(
+
+  const paginatedData = filteredTrainingData.slice(
     (currentPage - 1) * itemsPerPage,
     currentPage * itemsPerPage
   )
-
   useEffect(() => {
     setCurrentPage(1)
   }, [activeFilter])
 
   return (
     <>
-      <section className='bg-linear-to-bl from-primary/10 via-white to-secondary/20 py-16'>
+      <section className="py-14 md:py-16 lg:py-20" data-aos="fade-up">
         <div className="container">
-          <div className="max-w-3xl mb-10">
-            <h1 className='h3'>Upgrade Your Skills with {' '} <span className='text-primary'>Industry-Focused Training</span></h1>
-            <p>Learn practical, job-ready skills from experienced mentors. Join our hands-on training programs and build your career with confidence.</p>
+          <BlockTitle
+            title="Professional"
+            orangeText="Training Programs"
+            description="Industry-oriented courses with hands-on experience and placement support"
+            customClass="text-center max-w-3xl mx-auto mb-10 text-dark"
+          />
+          <div className="flex gap-6 mb-10 overflow-auto custom-scroll-blue pt-4 pb-6">
+            {Data.accordionButtonData.map((item, index) => (
+              <button onClick={() => setActiveFilter(item.accordionBtn)} className={`accordion-btn btn btn-blue ${activeFilter === item.accordionBtn ? 'active' : ''
+                }`} key={index}>{item.accordionBtn}</button>
+            ))}
           </div>
-          <div className="flex -mx-3 gap-y-7 flex-wrap">
-            <div className="px-3 w-full lg:w-1/5">
-              <div className="bg-white rounded shadow p-3">
-                <div className="flex lg:flex-wrap gap-3 overflow-x-auto custom-scroll pb-6">
-                  {Data.accordionButtonData.map((item, index) => (
-                    <button onClick={() => setActiveFilter(item.accordionBtn)} className={`accordion ${activeFilter === item.accordionBtn ? 'active' : ''
-                      }`} key={index}>{item.accordionBtn}</button>
-                  ))}
-                </div>
+          <div className="flex gap-y-7 flex-wrap -mx-3">
+            {filteredTrainingData.length === 0 ? (
+              <div className="w-full text-center py-2">
+                <h3 className="text-xl mb-0 font-semibold text-red">
+                  No training found
+                </h3>
+                <p className="text-secondary mt-2">
+                  Try selecting another category
+                </p>
               </div>
-            </div>
-            <div className="px-3 lg:w-[80%]">
-              <div className="flex flex-wrap gap-y-4 -mx-2">
-                {filteredData.length === 0 ? (
-                  <div className="w-full text-center py-2" data-aos="fade-down">
-                    <h3 className="text-xl mb-0 font-semibold text-red">
-                      No training found
-                    </h3>
-                    <p className="text-secondary mt-2">
-                      Try selecting another category
-                    </p>
-                  </div>
-                ) : (
-                  paginatedData.map((item, index) => (
-                    <div className="w-full sm:w-1/2 lg:w-1/3 px-2" key={index} data-aos="fade-right">
-                      <ServiceCard
-                        cardTitle={item.cardTitle}
-                        cardDescription={item.cardDescription}
-                        ctaText={item.ctaText}
-                        ctaLink={item.ctaLink}
-                        iconText={item.iconText}
-                        bgColor={item.bgColor}
-                        borderColor={item.borderColor}
-                        learnIconClass={item.learnIconClass}
-                        category={item.category}
-                        cardTitleClass={item.cardTitleClass}
-                        currentPage={currentPage}
-                      />
-                    </div>
-                  ))
-                )}
-              </div>
-              {/* Pagination Code */}
-              {totalPages > 1 && (
-                <div className="flex justify-center gap-2 mt-10">
-                  {/* Prev */}
-                  <button
-                    disabled={currentPage === 1}
-                    onClick={() => setCurrentPage((p) => p - 1)}
-                    className="pagination-btn"
-                  >
-                    Prev
-                  </button>
-
-                  {/* Pages */}
-                  {Array.from({ length: totalPages }, (_, i) => (
-                    <button
-                      key={i}
-                      onClick={() => setCurrentPage(i + 1)}
-                      className={`px-3 py-2 rounded ${currentPage === i + 1
-                        ? 'cursor-pointer bg-primary text-white'
-                        : 'cursor-pointer bg-primary/10 text-primary hover:bg-primary hover:text-white transition-all duration-300'
-                        }`}
-                    >
-                      {i + 1}
-                    </button>
-                  ))}
-
-                  {/* Next */}
-                  <button
-                    disabled={currentPage === totalPages}
-                    onClick={() => setCurrentPage((p) => p + 1)}
-                    className="pagination-btn"
-                  >
-                    Next
-                  </button>
+            ) : (
+              paginatedData.map((item, index) => (
+                <div className="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 px-3" data-aos="fade-right" key={index}>
+                  <TrainingCard
+                    cardTitle={item.cardTitle}
+                    time={item.time}
+                    timeIcon={item.timeIcon}
+                    userIcon={item.userIcon}
+                    students={item.students}
+                    cardLink={item.cardLink}
+                    currentPage={currentPage}
+                  />
                 </div>
-              )}
-            </div>
+              ))
+            )}
           </div>
+          {/* Pagination Code */}
+          {totalPages > 1 && (
+            <div className="flex justify-center gap-2 mt-10">
+              {/* Prev */}
+              <button
+                disabled={currentPage === 1}
+                onClick={() => setCurrentPage((p) => p - 1)}
+                className="pagination-btn"
+              >
+                Prev
+              </button>
+
+              {/* Pages */}
+              {Array.from({ length: totalPages }, (_, i) => (
+                <button
+                  key={i}
+                  onClick={() => setCurrentPage(i + 1)}
+                  className={`px-3 py-2 rounded ${currentPage === i + 1
+                    ? 'pagination-number'
+                    : 'cursor-pointer bg-primary/10 text-primary hover:bg-primary hover:text-white transition-all duration-300'
+                    }`}
+                >
+                  {i + 1}
+                </button>
+              ))}
+
+              {/* Next */}
+              <button
+                disabled={currentPage === totalPages}
+                onClick={() => setCurrentPage((p) => p + 1)}
+                className="pagination-btn"
+              >
+                Next
+              </button>
+            </div>
+          )}
         </div>
       </section>
     </>
---
+++ src/components/tabs/verticalTab.tsx
@@ -0,0 +1,162 @@
+'use client';
+import React, { useEffect, useState } from 'react'
+import ServiceCard from '../cards/serviceCard';
+import Data from '@/api/data.json'
+
+type Props = {}
+
+const VerticalTab = (props: Props) => {
+ const [activeFilter, setActiveFilter] = useState(
+  Data.accordionButtonData?.[0]?.accordionBtn || 'All Programs'
+ )
+ const filteredData =
+  activeFilter === 'All Programs'
+   ? Data.trainingPageData
+   : Data.trainingPageData.filter(
+    (item) => item.category === activeFilter
+   )
+
+ // Pagination Code
+ const [currentPage, setCurrentPage] = useState(1)
+ const itemsPerPage = 6
+ const totalPages = Math.ceil(filteredData.length / itemsPerPage)
+
+ const paginatedData = filteredData.slice(
+  (currentPage - 1) * itemsPerPage,
+  currentPage * itemsPerPage
+ )
+
+ useEffect(() => {
+  setCurrentPage(1)
+ }, [activeFilter])
+ return (
+  <>
+   <section className='bg-linear-to-bl from-primary/10 via-white to-secondary/20 py-16'>
+    <div className="container">
+     <div className="max-w-3xl mb-10">
+      <h1 className='h3'>Upgrade Your Skills with {' '} <span className='text-primary'>Industry-Focused Training</span></h1>
+      <p>Learn practical, job-ready skills from experienced mentors. Join our hands-on training programs and build your career with confidence.</p>
+     </div>
+     <div className="flex -mx-3 gap-y-7 flex-wrap">
+      <div className="px-3 w-full lg:w-1/5">
+       <div className="bg-white rounded shadow p-3">
+        <div className="flex lg:flex-wrap gap-3 overflow-x-auto custom-scroll pb-6">
+         {Data.accordionButtonData.map((item, index) => (
+          <button onClick={() => setActiveFilter(item.accordionBtn)} className={`accordion ${activeFilter === item.accordionBtn ? 'active' : ''
+           }`} key={index}>{item.accordionBtn}</button>
+         ))}
+        </div>
+       </div>
+      </div>
+      <div className="px-3 lg:w-[80%]">
+       <div className="flex flex-wrap gap-y-4 -mx-2">
+        {filteredData.length === 0 ? (
+         <div className="w-full text-center py-2" data-aos="fade-down">
+          <h3 className="text-xl mb-0 font-semibold text-red">
+           No training found
+          </h3>
+          <p className="text-secondary mt-2">
+           Try selecting another category
+          </p>
+         </div>
+        ) : (
+         paginatedData.map((item, index) => (
+          <div className="w-full sm:w-1/2 lg:w-1/3 px-2" key={index} data-aos="fade-right">
+           <ServiceCard
+            cardTitle={item.cardTitle}
+            cardDescription={item.cardDescription}
+            ctaText={item.ctaText}
+            ctaLink={item.ctaLink}
+            iconText={item.iconText}
+            bgColor={item.bgColor}
+            borderColor={item.borderColor}
+            learnIconClass={item.learnIconClass}
+            category={item.category}
+            cardTitleClass={item.cardTitleClass}
+            currentPage={currentPage}
+           />
+          </div>
+         ))
+        )}
+       </div>
+       {/* Pagination Code */}
+       {totalPages > 1 && (
+        <div className="flex justify-center gap-2 mt-10">
+         {/* Prev */}
+         <button
+          disabled={currentPage === 1}
+          onClick={() => setCurrentPage((p) => p - 1)}
+          className="pagination-btn"
+         >
+          Prev
+         </button>
+
+         {/* Pages */}
+         {Array.from({ length: totalPages }, (_, i) => (
+          <button
+           key={i}
+           onClick={() => setCurrentPage(i + 1)}
+           className={`px-3 py-2 rounded ${currentPage === i + 1
+            ? 'cursor-pointer bg-primary text-white'
+            : 'cursor-pointer bg-primary/10 text-primary hover:bg-primary hover:text-white transition-all duration-300'
+            }`}
+          >
+           {i + 1}
+          </button>
+         ))}
+
+         {/* Next */}
+         <button
+          disabled={currentPage === totalPages}
+          onClick={() => setCurrentPage((p) => p + 1)}
+          className="pagination-btn"
+         >
+          Next
+         </button>
+        </div>
+       )}
+      </div>
+     </div>
+    </div>
+   </section>
+   {/* Pagination Code */}
+   {totalPages > 1 && (
+    <div className="flex justify-center gap-2 mt-10">
+     {/* Prev */}
+     <button
+      disabled={currentPage === 1}
+      onClick={() => setCurrentPage((p) => p - 1)}
+      className="pagination-btn"
+     >
+      Prev
+     </button>
+
+     {/* Pages */}
+     {Array.from({ length: totalPages }, (_, i) => (
+      <button
+       key={i}
+       onClick={() => setCurrentPage(i + 1)}
+       className={`px-3 py-2 rounded ${currentPage === i + 1
+        ? 'pagination-number'
+        : 'cursor-pointer bg-primary/10 text-primary hover:bg-primary hover:text-white transition-all duration-300'
+        }`}
+      >
+       {i + 1}
+      </button>
+     ))}
+
+     {/* Next */}
+     <button
+      disabled={currentPage === totalPages}
+      onClick={() => setCurrentPage((p) => p + 1)}
+      className="pagination-btn"
+     >
+      Next
+     </button>
+    </div>
+   )}
+  </>
+ )
+}
+
+export default VerticalTab
